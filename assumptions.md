6cbaf286815297e13bc7757487d8f56004b058a5
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-02 06:40:18 +1100

    implemented simple test for channel_leave and removed email assumption with + delimiter in assumptions.md due to clarification

--- assumptions.md
+++ assumptions.md
@@ -1,5 +1,5 @@
 # Assumptions for the slackr Project
-- Emails with the plus `+` delimiter and the period `.` delimiter will be treated as different emails
+- Emails with the period `.` delimiter will be treated as different emails
 - The string `"THISISNOTATOKEN"` is not a valid token
 - Assuming that a non "valid channel" implies a non existent channel ID except when `InputError` is the only exception.
 - Assuming for `channel_leave()` that `InputError` is raised before `AccessError` in the case that the `channel_id` doesn't exist.
--- src/channel_leave_test.py
+++ src/channel_leave_test.py
@@ -1,14 +1,48 @@
 # Tests for the leave() function in channel.py
-# TODO: replace "VALIDTOKEN"s with an actual, valid token
+# TODO: need to make sure passwords, names etc. for auth_register are valid and will not return an error
 
 import pytest
-from channel import leave
+import channels as chs
+import channel as ch
+import auth
 from error import InputError, AccessError
+
+
+# Setting global variables
+login = auth.auth_register("test@test.tst", "password", "name", "lastname")
+u_id = login["u_id"]
+token = login["token"]
+
+# Create public channel with user (token) in it
+channel_id = chs.channels_create(token, "channel1", True)
+
+# Get details of the channel
+channel = ch.channel_details(token, channel_id)
+channel_name = channel["name"]
+channel_owner = channel["owner_members"]
+channel_members = channel["all_members"]  # array of {u_id, name_first, name_last}
+
+
+# Returns true if given user ID is part of the channel, else false
+def is_member(user_id):
+    return any([user_id == member["u_id"] for member in channel_members])
+
+
+###############################
+#            Tests            #
+###############################
 
 
 # Test for a normal input
 def test_leave_simple():
-    pass
+
+    # Check that user is actually part of the channel - if this fails, problem with dependencies
+    assert(is_member(u_id))
+
+    ch.channel_leave(token, channel_id)
+
+    # Assert that user is no longer a member
+    assert(not is_member(u_id))
 
 
 # Test if AccessError exceptions are raised
@@ -18,6 +52,11 @@ def test_leave_simple():
 def test_leave_access_error():
     # TODO: give an invalid token as an arg
     # TODO: give an authorised user that isn't part of the channel
+
+    # Create a second user that is not part of the channel
+    login2 = auth.auth_register("test2@test.tst", "password2", "name2", "lastname2")
+    u_id2 = login["u_id"]
+    token2 = login["token"]
 
     pass
 
