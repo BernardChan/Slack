17e24fb7bb40f867a3e71374f4a8617afda281c0
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-03 21:13:03 +1100

    Finished checking basic elements of register()

--- src/auth_test.py
+++ src/auth_test.py
@@ -1,45 +1,66 @@
 import pytest
-from auth.py import login
-from auth.py import logout
-from auth.py import register
+from auth import auth_login
+from auth import auth_logout
+from auth import auth_register
 from error import InputError, AccessError
 
+import sys
+sys.path.append('../')
+
 #Regular Expressions Module
+#Used for email regular expressions
 import re
 
-def test_register_short_password():
-    with pytest.raises(InputError) as e:
-        register("bill.gates@microsoft.com", "123", "Bill", "Gates")
 
-def test_register_valid_name():
-    register("bill.gates@microsoft.com", "123", "Bill", "Gates")
-
-def test_register_valid_first_name():
+"""
+auth_register() validation function. 
+"""
+#auth_register() Password Validation
+def test_auth_register_short_password():
     with pytest.raises(InputError) as e:
-        register("bill.gates@microsoft.com", "123", "B"*51, "Gates")
+        auth_register("bill.gates@microsoft.com", "123", "Bill", "Gates")
 
-def test_register_valid_first_name():
+def test_auth_register_valid_password():
+    auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+
+
+#auth_register() First Name Validation
+def test_auth_register_invalid_first_name():
     with pytest.raises(InputError) as e:
-        register("bill.gates@microsoft.com", "123", "Bill", "G"*51)
-    
-def test_email(email):
-    regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-    if(re.search(regex,email)):  
+        auth_register("bill.gates@microsoft.com", "123", "B"*51, "Gates")
+
+def test_auth_register_valid_first_name():
+    auth_register("bill.gates@microsoft.com", "123", "Bill", "Gates")
+
+
+#auth_register() Last Name Validation
+def test_auth_register_valid_last_name():
+    auth_register("bill.gates@microsoft.com", "123", "Bill", "Gates")
+
+def test_auth_register_invalid_last_name():
+    with pytest.raises(InputError) as e:
+        auth_register("bill.gates@microsoft.com", "123", "Bill", "G"*51)
+
+
+#auth_register() Email Validation
+def check_email(email):
+    regex = r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+    if(re.search(regex,email)): 
         #success needs do nothing right??
-        pass
+        auth_register(email, "123456", "Bill", "Gates")
     else:  
         with pytest.raises(InputError) as e:
-            
-if __name__ == '__main__' :  
-      
-    # Enter the email  
+            auth_register(email, "123456", "Bill", "Gates")
+       
+
+def test_register_valid_email():
     email = "bill.gates@microsoft.com"
-      
-    # calling run function  
-    check(email) 
+    check_email(email) 
   
-    email = "validemail@gmail.com"
-    check(email) 
-  
+def test_register_invalid_email():
     email = "billyboi.com"
-    check(email) 
+    check_email(email) 
+    
+    
+#python3 -m pytest auth_test.py
+#command for if forgotten or lost    
