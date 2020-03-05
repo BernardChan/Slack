32ff31c2c9f6174723a6c9a142f5003290f439a0
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-05 20:58:15 +1100

    refactored names to be more concise by removing get_. Also fixed a bug where args could not be passed into pytest fixtures by using a closure to capture the args

--- src/helper_functions/helpers.py
+++ src/helper_functions/helpers.py
@@ -3,10 +3,6 @@ import auth
 import channels as chs
 import channel as ch
 
-# TODO: address the warning since I'm using a pytest.fixture as a function in this file. It works fine,
-#  but need to see what tutor says.
-
-
 # Get Invalid IDs
 @pytest.fixture
 def invalid_channel_id():
@@ -24,20 +20,20 @@ def invalid_user_id():
 
 @pytest.fixture
 # Create a default member that doesn't have special permissions
-def get_member():
+def member():
     data = auth.auth_register("defaultmember@test.com", "password1", "default", "member")
     return data["u_id"], data["token"]
 
 # Create the slackr owner but isn't part of any channels
 @pytest.fixture
-def get_slackr_owner():
+def slackr_owner():
     slackr_owner = auth.auth_register("slackrownertest@test.tst", "password", "slackr", "owner")
     return slackr_owner["u_id"], slackr_owner["token"]
 
 
 # Create a channel owner
 @pytest.fixture
-def get_channel_owner():
+def channel_owner():
     chan_owner = auth.auth_register("ownertest2@test.tst", "password2", "channel", "owner")
     return chan_owner["u_id"], chan_owner["token"]
 
@@ -47,9 +43,9 @@ def get_channel_owner():
 
 # Create public channel with the channel owner as the sole person in it
 @pytest.fixture
-def get_public_channel():
+def public_channel():
     # TODO: verify that this function can call the other functions in this file
-    chan_owner_id, chan_owner_token = get_channel_owner()
+    chan_owner_id, chan_owner_token = channel_owner()
     channel_name = "publicChannel"
     channel_id = chs.channels_create(chan_owner_token, channel_name, True)
 
@@ -57,8 +53,8 @@ def get_public_channel():
 
 
 @pytest.fixture
-def get_private_channel():
-    chan_owner_id, chan_owner_token = get_channel_owner()
+def private_channel():
+    chan_owner_id, chan_owner_token = channel_owner()
     private_channel_name = "privateChannel"
     private_channel_id = chs.channels_create(chan_owner_token, private_channel_name, False)
 
@@ -68,9 +64,9 @@ def get_private_channel():
 # Get details of the public channel - returns "name", "owner_members", "all_members"
 # look up specification for usage and return types
 @pytest.fixture
-def get_channel_details():
-    chan_owner_id, chan_owner_token = get_channel_owner()
-    channel_name, channel_id = get_public_channel()
+def channel_details():
+    chan_owner_id, chan_owner_token = channel_owner()
+    channel_name, channel_id = public_channel()
     channel = ch.channel_details(chan_owner_token, channel_id)
 
     return channel["name"], channel["owner_members"], channel["all_members"]
@@ -79,37 +75,42 @@ def get_channel_details():
 # Get details of the private channel - returns "name", "owner_members", "all_members"
 # look up specification for usage and return types
 @pytest.fixture
-def get_private_channel_details():
-    chan_owner_id, chan_owner_token = get_channel_owner()
-    channel_name, channel_id = get_private_channel()
+def private_channel_details():
+    chan_owner_id, chan_owner_token = channel_owner()
+    channel_name, channel_id = private_channel()
     channel = ch.channel_details(chan_owner_token, channel_id)
 
     return channel["name"], channel["owner_members"], channel["all_members"]
+
 
 ######################################################################
 # Functions to check if a user is part of the private/public channel #
 ######################################################################
 
-
 # Returns true if given user ID is part of the channel, else false
+# Note: Closure (nested function) is used so that we can pass in args
+#   into is_member()/is_owner() when it's used as a fixture
 @pytest.fixture
-def is_member(user_id, is_public):
-    all_members = get_channel_details()[2]
-    p_all_members = get_private_channel_details()[2]
+def is_member():
+    def _params(user_id, is_public):
+        all_members = channel_details()[2]
+        p_all_members = private_channel_details()[2]
 
-    if is_public:
-        return any([user_id == person["u_id"] for person in all_members])
-    else:
-        return any([user_id == person["u_id"] for person in p_all_members])
+        if is_public:
+            return any([user_id == person["u_id"] for person in all_members])
+        else:
+            return any([user_id == person["u_id"] for person in p_all_members])
+    return _params
 
 
 @pytest.fixture
-def is_owner(user_id, is_public):
-    all_members = get_channel_details()[2]
-    p_all_members = get_private_channel_details()[2]
+def is_owner():
+    def _params(user_id, is_public):
+        all_members = channel_details()[2]
+        p_all_members = private_channel_details()[2]
 
-    if is_public:
-        return any([user_id == owner["u_id"] for owner in all_members])
-    else:
-        return any([user_id == owner["u_id"] for owner in p_all_members])
-
+        if is_public:
+            return any([user_id == owner["u_id"] for owner in all_members])
+        else:
+            return any([user_id == owner["u_id"] for owner in p_all_members])
+    return _params
