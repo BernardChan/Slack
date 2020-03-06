8f6306aa94f90d0b1d1e5f9891591d0fbe7b4a27
Julian Batongbacal <julian.batongbacal@gmail.com>
2020-03-06 18:02:06 +1100

    Added inputerror function for handle not between 3 and 20 characters

--- src/user_profile_sethandle_test.py
+++ src/user_profile_sethandle_test.py
@@ -10,11 +10,18 @@ from user import user_profile_sethandle
 from user import user_profile
 from error import InputError, AccessError
 
-# Pytest fixture to register a new test user
+# Pytest fixture to regiser test user 1
 @pytest.fixture
-def get_new_user():
-    data = auth_register("test.user@test.com", "password123", "fname", "lname")
-    return data["u_id"], data["token"]
+def user1():
+    data = auth_register("test.user1@test.com", "password123", "fname1", "lname1")
+    return data["token"], data["u_id"]
+    
+# Pytest fixture to regiser test user 2
+@pytest.fixture
+def user2():
+    data = auth_register("test.user2@test.com", "123password", "fname2", "lname2")
+    return data["token"], data["u_id"]
+
 
 # Helper function to assert that sethandle was successful
 def assert_sethandle_success(user_token, user_id, handle):
@@ -26,7 +33,20 @@ def assert_sethandle_success(user_token, user_id, handle):
 
 
 # Input error when handle is not between 3 and 20 characters
+def test_profile_set_handle(user1):
 
+    user_token, user_id = user1
+    with pytest.raises(InputError) as e:
+        user_profile_sethandle(user_token, "")
+        
+    with pytest.raises(InputError) as e:
+        user_profile_sethandle(user_token, "12")
+        
+    with pytest.raises(InputError) as e:
+        user_profile_sethandle(user_token, "a" * 21)
+        
+    with pytest.raises(InputError) as e:
+        user_profile_sethandle(user_token, "a" * 50)
 
 # Input error when handle is in use by another user
 
