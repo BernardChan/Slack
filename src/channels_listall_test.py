92b81023e232e45c3f3c1135046772a60710d6d8
Bernard Chan <z5210324@ad.unsw.edu.au>
2020-03-08 18:06:25 +1100

    fixed helper functions import, fixed syntax error with ==

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -1,15 +1,12 @@
 import channels
-import channel_helpers as ch
+import helper_functions.channel_helpers as ch
 
 # every token should print out the list of all channels
 def test_channels_listall():
-    assert channels_listall(ch.chan_owner_token) == 
-    [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert channels_listall(ch.chan_owner_token) == [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
 
-    assert channels_listall(ch.slackr_owner_token) == 
-    [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert channels_listall(ch.slackr_owner_token) == [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
 
-    assert channels_listall(ch.member_token) == 
-    [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert channels_listall(ch.member_token) == [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
 
 
