223c0a90cb51120b2121077788de8873e0c7d0da
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-09 10:56:55 +1100

    Added stub for auth_login_test

---
+++ src/auth_login_test.py
@@ -0,0 +1,13 @@
+# Tests for auth_login
+
+# Dependancies:
+    # user_profile()
+    #auth_register()
+    
+# Assumptions
+    # I can't test if a token is invalid in iteration 1 so I have to fudge it. 
+    
+# Tests
+    # If 
+
+def auth_login_
