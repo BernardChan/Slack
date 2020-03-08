51f0ba3960caf57a24b3a4760a3e3244939a1538
Bernard Chan <z5210324@ad.unsw.edu.au>
2020-03-08 17:39:17 +1100

    fixed syntax error, == must be on same line

--- src/channels_list_test.py
+++ src/channels_list_test.py
@@ -4,8 +4,7 @@ import channel_helpers as ch
 def test_channels_list():
     
     # checks channels list for a channel owner token
-    assert channels_list(ch.chan_owner_token) == 
-    [{ch.channel_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert channels_list(ch.chan_owner_token) == [{ch.channel_id, "channel1"}, {ch.private_channel_id, "channel2"}]
     
     # gives empty channel list for default member
     assert channels_list(ch.member_token) == {}    
