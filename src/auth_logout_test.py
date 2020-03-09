bddfa0fe187285b0a4d3df8aa9dbb75f59233083
Bernard Chan <z5210324@ad.unsw.edu.au>
2020-03-09 13:37:10 +1100

    finished tests, correctly called function, removed calling invalid token due to attribute error

--- src/auth_logout_test.py
+++ src/auth_logout_test.py
@@ -1,21 +1,18 @@
-import auth
+import auth as a
 import helper_functions.channel_helpers as ch
 
 def test_logout():
     # check logout for each token
-    assert auth_logout(ch.chan_owner_token) == True
-    assert auth_logout(ch.slackr_owner_token) == True
-    assert auth_logout(ch.member_token) == True
+    assert a.auth_logout(ch.chan_owner_token) == True
+    assert a.auth_logout(ch.slackr_owner_token) == True
+    assert a.auth_logout(ch.member_token) == True
     
     # attempt to logout an invalid token
-    
-    # token not in helper function
-    assert auth_logout(ch.token) == False
-    
+        
     # random string
-    assert auth_logout(Token) == False
+    assert a.auth_logout("Token") == False
     
     # no input 
-    assert auth_logout() == False
+    assert a.auth_logout() == False
     
 
