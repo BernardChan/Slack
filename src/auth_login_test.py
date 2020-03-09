d6054ba3cc8d89db0767df8d651ad31ed117ee40
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-09 18:44:55 +1100

    Complete auth_login_test, but I don't have auth.py in here for some reason so I ahve to fix that before confirming it is working

--- src/auth_login_test.py
+++ src/auth_login_test.py
@@ -1,5 +1,9 @@
 # Tests for auth_login
 
+# Dependancies:
+    # user_profile()
+    #auth_register()
+
 import pytest
 from auth import auth_login
 from auth import auth_logout
@@ -10,19 +14,48 @@ from error import InputError, AccessError
 import sys
 sys.path.append('../')
 
-# Dependancies:
-    # user_profile()
-    #auth_register()
+# Tests to run
+    # register then login success
+    # Rego not the same as login
+    # Login with wrong password error
+    # login with duplicate uid
+    # Register and login with two different requests and assert u_id is different
+    # Register and login with two users but with token not the u_id. 
     
-# Assumptions
-    # I can't test if a token is invalid in iteration 1 so I have to fudge it. 
+def test_auth_login_register_then_login_success():
+    register1 = auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+    login1 = auth_login("bill.gates@microsoft.com", "123456")
+    assert register1['u_id'] == login1['u_id']
     
-# Tests
-    # If token is valid
-    # If token is invalid
+def test_auth_login_no_user_found():
+    register1 = auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+    with pytest.raises(InputError) as e:
+        login1 = auth_login("bill.gates@microsoft.co", "123456")
+        
+def test_auth_login_wrong_password():
+    register1 = auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+    with pytest.raises(InputError) as e:
+        login1 = auth_login("bill.gates@microsoft.com", "wrong_password")
+    
+def test_auth_login_duplicate_id():
+    register1 = auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+    login1 = auth_login("bill.gates@microsoft.com", "123456")
+    register2 = auth_register("andy.gates@microsoft.com", "24681012", "Andy", "Gates")
+    login2 = auth_login("andy.gates@microsoft.com", "24681012")
+    with pytest.raises(InputError) as e:
+        assert register1['u_id'] != login1['u_id']
+        
+def test_auth_login_invalid_email():
+    register1 = auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+    with pytest.raises(InputError) as e:
+        login1 = auth_login("bill.gatemicrosoft.com", "123456")
+    
+def test_auth_login_different_tokens():
+    register1 = auth_register("bill.gates@microsoft.com", "123456", "Bill", "Gates")
+    login1 = auth_login("bill.gates@microsoft.com", "123456")
+    register2 = auth_register("andy.gates@microsoft.com", "24681012", "Andy", "Gates")
+    login2 = auth_login("andy.gates@microsoft.com", "24681012")
+    with pytest.raises(InputError) as e:
+        assert register1['token'] != login1['token']
 
-def auth_login_valid_token():
-    pass
-    
-def auth_login_invalid_token():
-    
+# python3 -m pytest auth_login_test.py
