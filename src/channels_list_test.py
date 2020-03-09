211ce10cffcf60b25b03a64917e175b3b165b61f
Bernard Chan <z5210324@ad.unsw.edu.au>
2020-03-09 17:42:07 +1100

    had to commit to switch branch, this still WIP

--- src/channels_list_test.py
+++ src/channels_list_test.py
@@ -1,13 +1,13 @@
-import channels
+import channels as c 
 import helper_functions.channel_helpers as ch
 
 def test_channels_list():
     
     # checks channels list for a channel owner token
-    assert channels_list(ch.chan_owner_token) == [{ch.channel_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert c.channels_list(ch.chan_owner_token) == [{ch.channel_id: "channel1"}, {ch.private_channel_id: "channel2"}]
     
     # gives empty channel list for default member
-    assert channels_list(ch.member_token) == {}    
+    assert c.channels_list(ch.member_token) == {}    
     
     # empty channel list for slackr owner
-    assert channels_list(ch.slackr_owner_token) == {}
+    assert c.channels_list(ch.slackr_owner_token) == {}
