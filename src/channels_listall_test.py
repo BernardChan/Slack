84b227be8d7975eb95d1da29eb8f9b687ad81cdf
Bernard Chan <z5210324@ad.unsw.edu.au>
2020-03-09 18:06:21 +1100

    final testing done, called functions correctly, correctly wrote list of dictionaries

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -1,12 +1,11 @@
-import channels
+import channels as c
 import helper_functions.channel_helpers as ch
 
 # every token should print out the list of all channels
 def test_channels_listall():
-    assert channels_listall(ch.chan_owner_token) == [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert c.channels_listall(ch.chan_owner_token) == [{'ch.chan_owner_id': 1, 'name': "channel1"}, {'ch.private_channel_id': 1, 'name': "channel2"}]
 
-    assert channels_listall(ch.slackr_owner_token) == [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
+    assert c.channels_listall(ch.slackr_owner_token) == [{'ch.chan_owner_id': 1, 'name': "channel1"}, {'ch.private_channel_id': 1, 'name': "channel2"}]
 
-    assert channels_listall(ch.member_token) == [{ch.chan_owner_id, "channel1"}, {ch.private_channel_id, "channel2"}]
-
+    assert c.channels_listall(ch.member_token) == [{'ch.chan_owner_id': 1, 'name': "channel1"}, {'ch.private_channel_id': 1, 'name': "channel2"}]
 
