1181d80699828d3dcad6c98041c83be8e7d5ccaf
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-09 15:33:20 +1100

    Added comment

--- src/auth_login_test.py
+++ src/auth_login_test.py
@@ -1,5 +1,15 @@
 # Tests for auth_login
 
+import pytest
+from auth import auth_login
+from auth import auth_logout
+from auth import auth_register
+from user import user_profile
+from error import InputError, AccessError
+
+import sys
+sys.path.append('../')
+
 # Dependancies:
     # user_profile()
     #auth_register()
@@ -8,6 +18,11 @@
     # I can't test if a token is invalid in iteration 1 so I have to fudge it. 
     
 # Tests
-    # If 
+    # If token is valid
+    # If token is invalid
 
-def auth_login_
+def auth_login_valid_token():
+    pass
+    
+def auth_login_invalid_token():
+    
