667fa6fe6860932becae6d0ab328a0bb977a936b
Julian Batongbacal <julian.batongbacal@gmail.com>
2020-03-15 23:08:24 +1100

    Set up user.py file. Made function for user/profile, currently unsure on whether it would work or not needs peer review. Currently assumes that a token is a user id because we haven't decided.

---
+++ src/user.py
@@ -0,0 +1,37 @@
+# user/... functions for slackr app
+
+from error import AccessError, InputError
+import database_files.database as db
+
+# Helper function for checking if a token is valid or not
+# Assumes that a token is the user's userid
+def valid_token(token):
+    # Go through all user ids and check if it exists
+    for user in db.users:
+        if user["u_id"] == token:
+            return True
+    return False
+
+# USER/PROFILE
+# For a valid user, returns information about their user id, email, first name, last name, and handle
+# Input Error for:
+    # User with u_id is not a valid user
+# Access Error for:
+    # Token is not valid
+def user_profile(token, u_id):
+    
+    # Raise an AccessError if not a valid token
+    if not valid_token(token):
+        raise AccessError(description="Invalid token!")
+    
+    # Go through all users and collect the correct user dictionary
+    profile = {}
+    for user in db.users:
+        if user["u_id"] == u_id:
+            profile = user
+
+    # If profile is empty, u_id is invalid because user does not exist, so raise InputError
+    if profile == {}:
+        raise InputError(description="Invalid user id!")
+
+    return profile
