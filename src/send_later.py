485c071373587b9a8dda5f7550e350914f29d3c8
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-16 16:18:15 +1100

    added functional send_later() that works by creating adding new messages to a queue, then creating a sched object from whatever is added to the queue. This is done in another thread to prevent blocking

--- src/send_later.py
+++ src/send_later.py
@@ -11,54 +11,6 @@ import time
 import sched
 
 
-channels = [
-    {"channel_id": 0,
-     "name": "foo"},
-
-    {"channel_id": 1,
-     "name": "bar"},
-
-    {"channel_id": 2,
-     "name": "baz"},
-
-]
-foo={ # TODO delete later
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-            }
-        ],
-    }
-
-# Global Dictionary Containing every user in the slackr server
-DATABASE = {
-    "users": [
-        {
-            "token": "invalid toshi token",
-            "u_id": 0,
-            "name_first": "Toshi",
-            "name_last": "Tabata",
-        },
-        {
-            "token": "INVALID",
-            "u_id": 1,
-            "name_first": "Hayden",
-            "name_last": "Smith",
-        }
-    ]
-}
-
-
 # gets the user dictionary based on the key given
 # If user is not found, return None
 # TODO: should I make an exception or try/catch for when the wrong key_type is given?
@@ -113,26 +65,61 @@ def is_message_valid(message, channel_id):
     # TODO: add Access Error for when a user hasn't joined a channel they're posting to
 
 
+# TODO: delete this message_send and import the real one. This is for testing purposes only
 def message_send(message):
     print(message)
 
 
-def set_sched(time_sent, message):
-    s = sched.scheduler(time.time, time.sleep)
-    s.enterabs(time_sent, 0, message_send, [message])
-    s.run()
+# Message queue for sending a message later
+send_later_queue = [{"message": "hi", "time_sent": time.time(), "priority": time.time()}]
+do_work = threading.Event()
 
 
-def send_later(token, channel_id, message, time_sent):
-    is_message_valid(message, channel_id)
+# TODO: import the real message_send function
+# Dequeues send_later_queue whenever a new item is added to it
+def set_sched():
+    global send_later_queue
+    global do_work
+    while True:
 
-    t = threading.Thread(target=set_sched, args=(time_sent, message))
+        # If there are items to dequeue
+        if send_later_queue:
+
+            # get the time_sent, message and priority from send_later_queue
+            time_sent, message, priority = send_later_queue[0]["time_sent"], send_later_queue[0]["message"], send_later_queue[0]["priority"]
+            del(send_later_queue[0])
+
+            # Create a schedule object to be run at a later date
+            s = sched.scheduler(time.time, time.sleep)
+            s.enterabs(time_sent, priority, message_send, [message])
+            s.run()
+        else:
+            do_work = threading.Event()
+            do_work.wait()
+
+
+# TODO: move this to server.py
+# Creates a thread for setting a schedule
+def start_thread_helper():
+    t = threading.Thread(target=set_sched)
     t.start()
 
 
-if __name__ == "__main__":
-    send_later(0,0, "2", time.time() + 10)  # sends this second
-    send_later(0,0, "1", time.time() + 5)  # sends this first
+# TODO: add error checking
+# Adds a message to be sent at a later date
+def send_later(token, channel_id, message, time_sent):
+    # is_message_valid(message, channel_id)
+    global do_work
+    send_later_queue.append({"message": message, "time_sent": time_sent, "priority": time.time()})
+    do_work.set()
 
-    for i in range(0, 2000):
-        send_later(0, 0, "3", time.time() + 1)  # sends this first
+
+if __name__ == "__main__":
+    start_thread_helper()  # Should only be called once
+    curr = time.time()
+    for i in range(100):
+        send_later(0, 0, str(i), curr + 5)
+
+    time.sleep(20)
+    send_later(0, 0, "yoyoyo", time.time())
+
