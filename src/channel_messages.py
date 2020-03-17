37902d2983d197ad747142ed02c485182ee01306
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-17 14:46:01 +1100

    added error checking for channel_messages (implementation for channel_messages was already done)

--- src/channel_messages.py
+++ src/channel_messages.py
@@ -1,7 +1,8 @@
 # Function implementation for channel/messages(token, channel_id, start)
 
-# TODO: Create function stubs in database that just gives us an interface to get data from DATABASE
-# if database structure changes, only the function has to be changed
+import database_files.database_retrieval as db
+import helper_functions.interface_function_helpers as help
+from error import InputError
 
 
 # Finish is the ending index for the list slice, whereas
@@ -10,10 +11,13 @@
 def get_finish_and_end(start, messages):
     end = start + 50
     finish = end
+    num_messages = len(messages)
+    if start >= num_messages:
+        raise InputError("Start given was greater than the number of messages in the channel")
 
     # set finish to -1 if we don't have 50 messages to send back
-    if len(messages) < end:
-        print("no more messages to display")
+    if num_messages < end:
+
         end = -1
 
     if end == -1:
@@ -23,7 +27,12 @@ def get_finish_and_end(start, messages):
 
 
 def channel_messages(token, channel_id, start):
-    # TODO: get messages
+
+    # Check Errors
+    help.check_channel_validity(channel_id)
+    help.is_user_valid_channel_member(token, channel_id)
+
+    messages = db.get_channel_messages(channel_id)
 
     # Get the finish index for messages and end index for return value
     finish, end = get_finish_and_end(start, messages)
