3267b17002df42dffde99cad59e8cbbb530aecb2
Julian Batongbacal <julian.batongbacal@gmail.com>
2020-03-20 16:48:19 +1100

    Implemented channels/list. Very simple, just calls the get_user_channels_by_key function from database_retrieval and returns the list output.

---
+++ src/interface_functions/channels_list.py
@@ -0,0 +1,16 @@
+from error import InputError, AccessError
+from database_files.database_retrieval import get_user_channels_by_key
+
+# CHANNELS/LIST
+# Provides a list of all channels (and their associated etails) that the user is part of
+# Only raises AccessError for invalid token
+def channels_list(token):
+
+    # Raise an access error if not a valid token
+    # TODO
+
+    # Get the list of all channels the user is part of
+    channels = get_user_channels_by_key("token", token)
+    
+    # return the list
+    return { channels }
