7e515ccede42e448c9bb240a4ca3cc41b41f8ffd
Bernard Chan <z5210324@ad.unsw.edu.au>
2020-03-20 13:45:13 +1100

    made simplicity adjustments to function, utilised helper functions for exception testing, token validity function still not implemented

--- src/channel_functions/channel_leave.py
+++ src/channel_functions/channel_leave.py
@@ -1,5 +1,5 @@
-from error import AccessError, InputError
-from database_files.database_retrieval import get_channels, is_user_in_channel
+import database_files.database_retrieval as db
+import helper_functions.interface_function_helpers as help
 from database_files.database import DATABASE as DATABASE
 
 def channel_leave(token, channel_id):
@@ -9,22 +9,15 @@ def channel_leave(token, channel_id):
         pass
         
     # check if channel is valid
-    channels = get_channels()
-    
-    for channel in channels:
-        if channel['channel_id'] == channel_id:
-            pass
-        else:
-            raise InputError("The channel_id is not a valid channel")
+    help.check_channel_validity(channel_id)
             
     # check if user is a member of the channel
-    if is_user_in_channel('token', token, channel_id) == False:
-        raise AccessError("Authorised user is not a member of the channel")
+    help.is_user_valid_channel_member(token, channel_id)
         
     # remove the authorised user from the channel
     mem = DATABASE['channels'][channel_id]['members']
     for i in range(len(mem)):
-        if chan[i]['token'] == token:
+        if mem[i]['token'] == token:
             del mem[i]
             break
 
