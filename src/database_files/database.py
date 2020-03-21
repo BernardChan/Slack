b868508e403e9c61be681d4760f89e89df2de8cc
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-21 15:37:53 +1100

    Clear_database added, unpickle added, getuser/getmessage/getchannel moved to here, register database backend added

--- src/database_files/database.py
+++ src/database_files/database.py
@@ -9,87 +9,115 @@ DATABASE = {
     "channels": [],
 }
 
+DATABASE_EMPTY = {
+    "users": [],
+    "messages": [],
+    "channels": [],
+}
+
+unpickle = False
+
 # Example of what the database will look like.
 # If you have additions, add them, make a merge request, and post it on slack.
-# Make sure all the keys are there when you're adding to this
 # Additions:
 # - "token" key added to "user" dictionary
 # - "channel_id" key added to "message" dictionary
-# - "permission_id" key added to "user" dictionary (1 = owner/admin, 2 = member)
 # - "members" key added to "channels" dictionary
-# - "standup" key added to "channels" dictionary - is a boolean for whether a standup is active
-MOCK_DATA = {
-    "users": [
-        {"u_id": 0, "email": "asd@asd.com", "name_first": "first0", "name_last": "last0", "handle_str": "handle0", "token": 0, "permission_id": 1},
-        {"u_id": 1, "email": "asd1@asd.com", "name_first": "first1", "name_last": "last1", "handle_str": "handle1", "token": 1, "permission_id": 2},
-        {"u_id": 2, "email": "asd2@asd.com", "name_first": "first2", "name_last": "last2", "handle_str": "handle2", "token": 2, "permission_id": 2}
-    ],
-
-    "messages": [
-        {
-            "message_id": 0,
-            "u_id": 0,
-            "message": "str",
-            "time_created": 0,
-            "reacts": {"react_id": 0, "u_ids": [0, 1, 2], "is_this_user_reacted": False},
-            "is_pinned": False,
-            "channel_id": 0,
-        },
-        {
-            "message_id": 1,
-            "u_id": 1,
-            "message": "1str",
-            "time_created": 1,
-            "reacts": {"react_id": 1, "u_ids": [0, 1, 2], "is_this_user_reacted": False},
-            "is_pinned": False,
-            "channel_id": 1},
-
-        {
-            "message_id": 2,
-            "u_id": 2,
-            "message": "2str",
-            "time_created": 2,
-            "reacts": {"react_id": 1, "u_ids": [0, 1, 2], "is_this_user_reacted": False},
-            "is_pinned": False,
-            "channel_id": 2
-        }
-    ],
-
-    "channels": [
-        {
-            "channel_id": 0,
-            "name": "0",
-            "members": [{"u_id": 0, "name_first": "first0", "name_last": "last0", "token": 0}],
-            "standup": {"active": False, "msg_queue": "", "time_finish": None}
-        },
-        {
-            "channel_id": 1,
-            "name": "1",
-            "members": [{"u_id": 1, "name_first": "first1", "name_last": "last1", "token": 1},
-                        {"u_id": 0, "name_first": "first0", "name_last": "last0", "token": 0}],
-            "standup": {"active": False, "msg_queue": "", "time_finish": None}
-        },
-
-        {
-            "channel_id": 2,
-            "name": "2",
-            "members": [{"u_id": 2, "name_first": "first2", "name_last": "last2", "token": 2}],
-            "standup": {"active": False, "msg_queue": "", "time_finish": None}
-        }
-    ],
-}
+# 21/03/20 Additions
+    # - "permission_id added to user. 1 for owner and 2 for everyone else
 
 
-# Saves the current database_files
+"""
+----------------------------------------------------------------------------------
+Query Functions
+----------------------------------------------------------------------------------
+""" 
+# These functions full information from the database.
+
+# Returns messages list
+def get_messages():
+    global DATABASE
+    return DATABASE["messages"]
+
+# Returns channels list
+def get_channels():
+    global DATABASE
+    return DATABASE["channels"]
+
+# Returns users list
+def get_users():
+    global DATABASE
+    return DATABASE["users"]
+    
+
+"""
+----------------------------------------------------------------------------------
+Database Functions
+----------------------------------------------------------------------------------
+""" 
+# These functions support the large scale operation of the database.
+
 def pickle_database():
-    with open("../database_files/database.p", "wb") as FILE:
-        pickle.dump(MOCK_DATA, FILE)
-
+    if path.exists("../database_files/database.p"):
+        with open("../database_files/database.p", "wb") as FILE:
+            pickle.dump(DATABASE, FILE)
+    else:
+        filename = "../database_files/database.p"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open("../database_files/database.p", "wb") as FILE:
+            pickle.dump(DATABASE, FILE)
+    unpickle = False
 
 # Restores the database_files from last save
 def unpickle_database():
     global DATABASE
-    DATABASE = pickle.load(open("../database_files/database.p", "rb"))
+    if path.exists("../database_files/database.p"):
+        DATABASE = pickle.load(open("../database_files/database.p", "rb"))
+    else:
+        DATABASE = {
+            "users": [],
+            "messages": [],
+            "channels": [],
+        }
+    unpickle = True
+        
+# Function to clear the database
+def clear_database():
+    global DATABASE
+    global DATABASE_EMPTY
+    DATABASE = DATABASE_EMPTY
+    pickle_database()
+    
+"""
+----------------------------------------------------------------------------------
+Data Operation Functions
+----------------------------------------------------------------------------------
+""" 
+# These functions actually change data in the database.
+    
+# Adding user.
+def add_user_to_database(email, password, name_first, name_last, handle, token, u_id):
+    if not unpickle:
+        unpickle_database()
+    global DATABASE
+    permission_id = 0
+    if u_id == 1:
+        permission_id = 1
+    else:
+        permission_id = 2
+    new_user = {
+        "u_id": u_id, 
+        "email": email, 
+        "password": password,
+        "name_first": name_first, 
+        "name_last": name_last, 
+        "handle_str": handle, 
+        "token": token,
+        "permission_id": permission_id
+    }
+
+    DATABASE['users'].append(new_user)
+    pickle_database()
 
 
 # pickle_database()
