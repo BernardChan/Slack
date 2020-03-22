e089eed07d994efd218620f8021270592b119cf1
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-22 19:40:32 +1100

    removed debugging print statements

--- src/server.py
+++ src/server.py
@@ -68,5 +68,7 @@ def standup_send():
     return dumps(su.standup_send(token, channel_id, message))
 
 
+
+
 if __name__ == "__main__":
     APP.run(port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
