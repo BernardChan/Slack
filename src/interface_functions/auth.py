361574406acc25787a678076e40d3169e6bb1968
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-24 00:28:07 +1100

    added auth_logout function

--- src/interface_functions/auth.py
+++ src/interface_functions/auth.py
@@ -1,3 +1,29 @@
+"""
+ auth.py
+# This function takes in input from the user, validates it, checks against the database, then authenticates them if everything is correct.
+
+"""
+# Usage
+    # Used for Validation of input only. 
+
+# Additions:
+# 21/03/20
+    # - register finished
+# 23/03/20
+    # - Login and Logout
+
+from error import AccessError, InputError
+import database_files.database as db
+import database_files.database_retrieval as dr
+import helper_functions.auth_helper as ah
+import pytest
+import time
+
+"""
+----------------------------------------------------------------------------------
+User Interface Functions
+----------------------------------------------------------------------------------
+"""     
 def auth_login(email, password):
     return {
         'u_id': 1,
@@ -5,9 +31,8 @@ def auth_login(email, password):
     }
 
 def auth_logout(token):
-    return {
-        'is_success': True,
-    }
+    is_success = db.logout_user(token)
+    return is_success
 
 def auth_register(email, password, name_first, name_last):
     return {
