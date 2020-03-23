00b7470d6c712bb41df5f1227d7809b5652bd953
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-24 00:09:28 +1100

    Moved all other functions apart from register away from this request to their own seperate branches

--- src/interface_functions/auth.py
+++ src/interface_functions/auth.py
@@ -3,6 +3,15 @@
 # This function takes in input from the user, validates it, checks against the database, then authenticates them if everything is correct.
 
 """
+# Usage
+    # Used for Validation of input only. 
+
+# Additions:
+# 21/03/20
+    # - register finished
+# 23/03/20
+    # - Login and Logout
+
 from error import AccessError, InputError
 import database_files.database as db
 import database_files.database_retrieval as dr
@@ -23,7 +32,7 @@ def auth_register(email, password, name_first, name_last):
     ah.validate_name_last(name_last)
     
     # Stage 2 - check database
-    if dr.is_duplicate_check("email", email):
+    if dr.is_duplicate("email", email):
         raise InputError(description = 'Email address is already being used by another user')
     
     # Stage 3 - Generate input transformations
@@ -43,32 +52,17 @@ def auth_register(email, password, name_first, name_last):
     }
     return register_dict
      
- 
         
 def auth_login(email, password):
-    # Validate Email
-    ah.validate_email(email)
-    user_rec =  dr.get_users_by_key("email", email)
-    if user_rec == []:
-        raise InputError(description = 'Email entered does not belong to a user')
-    
-    # Validate Password
-    hash_pw = ah.hash_data(password)
-    if hash_pw != user_rec[0]["password"]:
-        raise InputError(description = 'Password is not correct')
+    return {
+        'u_id': 1,
+        'token': '12345',
+    }
 
-    # Create and assign token to database
-    new_token = ah.get_valid_token(email)
-    login_dict = db.login_user(email, new_token)
-
-    return login_dict
-        
-        
 def auth_logout(token):
     return {
         'is_success': True,
     }
-
     
 if __name__ == '__main__':
     pass
