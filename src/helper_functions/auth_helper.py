88517d21ca43684108efc1a888427d3348c08427
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-23 20:34:55 +1100

    Fixed u_id and create handle

--- src/helper_functions/auth_helper.py
+++ src/helper_functions/auth_helper.py
@@ -65,14 +65,17 @@ def create_handle(name_first, name_last):
     handle = handle.lower()
     if len(handle) > 20:
         handle = handle[0:20]
-    while dr.is_duplicate_check("handle_str", handle):
-        hashed_input = hash_data(handle).upper()
+    while dr.duplicate_check("handle_str", handle):
+        hashed_input = hash_data(handle).lower()
         character = handle[0:-1] + hashed_input[1]
         handle = character
     return handle
     
  
 def get_u_id():
-    u_id_last = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    u_id_last = len(database.get_users())
+    while dr.duplicate_check("u_id", u_id_last):
+        u_id_last += 1
+    # u_id_last = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     return u_id_last
 
