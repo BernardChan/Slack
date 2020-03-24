c1daebdc90d9ef021049162f9f09887362666fb6
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-24 18:46:50 +1100

    Removed token generation from auth_register and moved it to database

--- src/interface_functions/auth.py
+++ src/interface_functions/auth.py
@@ -13,11 +13,10 @@
     # - Login and Logout
 
 from error import AccessError, InputError
-import database_files.database as db
+import database_files.database_update as du
 import database_files.database_retrieval as dr
 import helper_functions.auth_helper as ah
 import pytest
-import time
 
 """
 ----------------------------------------------------------------------------------
@@ -34,24 +33,14 @@ def auth_register(email, password, name_first, name_last):
     # Stage 2 - check database
     if dr.is_duplicate("email", email):
         raise InputError(description = 'Email address is already being used by another user')
-    
-    # Stage 3 - Generate input transformations
-    token = ah.get_valid_token(email)
-    u_id_returned = ah.get_u_id()
-    
+
     # Stage 4 - Store all user information in the database
-    db.add_user_to_database(email, \
-        ah.hash_data(password), name_first, name_last, \
-        ah.create_handle(name_first, name_last), \
-        token, u_id_returned \
+    u_id_returned = ah.get_u_id()
+    register_dict = du.add_user_to_database(email, ah.hash_data(password),\
+        name_first, name_last, ah.create_handle(name_first, name_last), \
+        u_id_returned \
     )
-    
-    register_dict = {
-        'u_id' : u_id_returned,
-        'token' : token
-    }
     return register_dict
-     
         
 def auth_login(email, password):
     return {
