19a89371fbd5596724d168f39a0729bcf63c30b5
Daniel Koenen <z5246763@ad.unsw.edu.au>
2020-03-24 18:58:41 +1100

    Missed a file to add: database.py

--- src/database_files/database.py
+++ src/database_files/database.py
@@ -1,6 +1,6 @@
 """
  database.py
-# This function stores input given from a file. Preserving the state by pickling the results and unpickling it when data needs to be added. All of the functions that directly access data are also added here. 
+# This function stores database files. Preserving the state by pickling the results and unpickling it when data needs to be added. 
 
 """
 # Usage
@@ -24,7 +24,7 @@
 import pickle
 from os import path
 import os
-from database_files.database_retrieval import get_users
+# from database_files.database_retrieval import get_users
 
 
 DATABASE = {
@@ -33,6 +33,7 @@ DATABASE = {
     "channels": [],
 }
 
+unpickle = False
 
 """
 ----------------------------------------------------------------------------------
@@ -68,66 +69,5 @@ def clear_database():
     "channels": [],
 }  
     pickle_database()
-
-"""
-----------------------------------------------------------------------------------
-Data Entry Functions
-----------------------------------------------------------------------------------
-"""    
-# Adding user.
-def add_user_to_database(email, password, name_first, name_last, handle, token, u_id):
    
-    unpickle_database() # delete
-    global DATABASE
-
-    permission_id = 0
-    if len(get_users()) == 0:
-        permission_id = 1
-    else:
-        permission_id = 2
-    
-    new_user = {
-        "u_id": u_id, 
-        "email": email, 
-        "password": password,
-        "name_first": name_first, 
-        "name_last": name_last, 
-        "handle_str": handle, 
-        "token": token,
-        "permission_id": permission_id
-    }
-
-    DATABASE['users'].append(new_user)
-    pickle_database()
-    
-    
-    
-def login_user(email, token):
-    unpickle_database() # delete
-    global DATABASE
-
-    for user in get_users():
-        if user["email"] == email:
-            DATABASE['users'][user['u_id'] - 1]['token'] = token
-            u_id = user['u_id']
-
-    pickle_database()
-    return {
-        'u_id': u_id,
-        'token': token,
-    }
-    
-    
-def logout_user(token):
-    unpickle_database() # delete
-    global DATABASE
-
-    is_success = False
-    for user in get_users():
-        if user["token"] == token:
-            DATABASE['users'][user['u_id'] - 1]['token'] = ""
-            is_success = True
-    pickle_database()
-    return is_success
-    
 unpickle_database
