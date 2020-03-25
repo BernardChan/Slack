cb9fc24fdef5c6eb2b8daeaf8189ba2d2aa2203e
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-25 16:00:28 +1100

    added comments to explain the channel_leave_test.py file

--- src/channel_leave_test.py
+++ src/channel_leave_test.py
@@ -7,17 +7,38 @@ import database_files.database_retrieval as db
 #   - Return value from channel/leave was correct
 #   - User was correctly removed from the database
 
+# =============================== README ===============================
+# I'm making this file extra verbose to explain exactly what is happening
+# Channel leave's specification looks like this:
 
-# Check that the user actually left the channel
+# HTTP Route    | HTTP Method |     Parameter       |  Return Type
+# channel/leave |     POST    | (token, channel_id) |      {}
+
+
+# Check that the user left the channel
 def test_channel_leave_simple():
+
+    # Reset the database between tests so we don't have to restart the server
     ch.reset()
+
+    # Calling the helper files to get the PARAMETERS needed for channel_leave(token, channel_id)
     u_id, token = ch.get_channel_owner()
     channel_id = ch.create_public_channel(token)
 
+    # PARAMETER for the function needs to be given as a dictionary
     data = {
         "token": token,
         "channel_id": channel_id
     }
 
+    # 1. Choose the function based on the HTTP Method
+    #       (channel_leave is POST, so we used make_post_request())
+    # 2. Give the function your HTTP Route as a string
+    #       (channel_leave uses channel/leave, so we give "channel/leave")
+    # 3. Give it your data as a dictionary
+    #       (channel_leave uses token, channel_id so I give {"token": ..., "channel_id":...}
+    # 4. Assert that the HTTP METHOD (post) request returned the correct RETURN TYPE == {}
     assert ch.make_post_request("channel/leave", data) == {}
+
+    # Check that the user was removed correctly
     assert not db.is_user_in_channel("token", token, channel_id)
