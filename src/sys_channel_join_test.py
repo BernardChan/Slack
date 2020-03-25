b2489042daeae45a3b0d279d0e0bcf9571a05e5f
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-03-25 16:35:13 +1100

    added system test for channel_join and fixed grammar in assumptions.md

--- src/sys_channel_join_test.py
+++ src/sys_channel_join_test.py
@@ -2,4 +2,17 @@ import helper_functions.system_test_helper_file as ch
 
 
 def test_channel_join_simple():
+    # Reset database and get relevant inputs
     ch.reset()
+    token = ch.get_channel_owner()[0]
+    channel_id = ch.create_public_channel(token)
+
+    data = {
+        "token": token,
+        "channel_id": channel_id
+    }
+
+    # Asert that the return value was correct and
+    # that the user was added as a member
+    assert ch.make_post_request("channel/join", data) == {}
+    assert ch.is_member(token, channel_id)
