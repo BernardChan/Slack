fc167a7e8fa8b4a4167d66f44c9e88e2792f3a99
z5280803 <z5280803@cse.unsw.edu.au>
2020-03-29 13:19:07 +1100

    fixed message tests to preven terroring out for no reason

--- src/tests/message_send_test.py
+++ src/tests/message_send_test.py
@@ -2,7 +2,7 @@ import helper_functions.test_helper_file as ch
 from interface_functions import channel, message
 from error import InputError
 
-#  # 
+# # Test is bugged. Trying to get messages when none exists.
 # def test_message_send():
     
 #     channel_messages = channel.channel_messages(ch.chan_owner_token, ch.channel_id, 0)
@@ -10,11 +10,7 @@ from error import InputError
 #     m_id = channel_messages_list[0]["message_id"]
     
 #     assert message.message_send(ch.chan_owner_token, ch.channel_id, message) == m_id
-    
-# def send_input_error():    
-#     if len(message) > 1000:
-#         raise(InputError)
-  
+
 
     
     
