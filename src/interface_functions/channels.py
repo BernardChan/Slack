e9c4cb1ceaa77529b680925b86ba5b0bd106126e
Julian Batongbacal <julian.batongbacal@gmail.com>
2020-03-29 12:37:02 +1100

    Added check for valid token to each function.

--- src/interface_functions/channels.py
+++ src/interface_functions/channels.py
@@ -3,30 +3,28 @@ import time
 import database_files.database_retrieval as db
 from database_files.database_retrieval import get_user_channels_by_key
 from database_files.database_retrieval import get_channels
+from helper_functions.interface_function_helpers import is_valid_token
 
 def channels_list(token):
     # Raise an access error if not a valid token
-    # TODO
-
+    is_valid_token(token)
     # Get the list of all channels the user is part of
     channels = get_user_channels_by_key("token", token)
-
     # return the list
     return {"channels": channels}
 
 
 def channels_listall(token):
     # Raise an access error if not a valid token
-    # TODO
-
+    is_valid_token(token)
     # Get the list of all channels the user is part of
     channels = get_channels()
-
     # return the list
     return {"channels": channels}
 
 
 def channels_create(token, name, is_public):
+    is_valid_token(token)
     if len(name) > 20:
         raise InputError("Channel name cannot be more than 20 characters long")
 
