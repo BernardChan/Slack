71b2f053a27fa88e321069f0e9e0976708949bbb
Julian Batongbacal <julian.batongbacal@gmail.com>
2020-04-15 15:33:27 +1000

    Made image saving more concise

--- src/interface_functions/user.py
+++ src/interface_functions/user.py
@@ -193,7 +193,6 @@ def user_profile_uploadphoto(token, img_url, x_start, y_start, x_end, y_end): # 
     u_id = get_users_by_key("token", token)[0]["u_id"]
     # save the image to database_files/user_images/{u_id}.jpg
     dirname = os.path.dirname(__file__)
-    img = img.save(os.path.join(dirname, "database_files/user_images/", f"{u_id}.jpg"))
-
+    img.save(os.path.join(dirname, "database_files/user_images/", f"{u_id}"), "JPEG")
 
     return {}
