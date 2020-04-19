22a9a93e554c675e1fe98d79d6dc97b072ae3961
Toshi-Tabata <48075791+Toshi-Tabata@users.noreply.github.com>
2020-04-19 10:06:16 +1000

    added randomiser for hangman word

--- src/interface_functions/hangman.py
+++ src/interface_functions/hangman.py
@@ -1,3 +1,5 @@
+import wikiquote
+import random
 from error import InputError
 import database_files.database_retrieval as db
 
@@ -10,20 +12,53 @@ channels[channel:{"hangman":
     "guessed": [" "],  # letters that have been guessed
     "hangmanWord": "word",
     "guess": guessLetter,  # currently guessed letter
-    "active": False
+    "active": False,
+    "is_winner": False
 }
 
 """
 
 
+# 1. Get a random quote
+# 2. Split each word to create a list of words
+# 3. Remove non-letter characters
+# 4. Remove words < 5 letters long - probably can't fail since most quotes contain > 5 letter words
+# 5. Pick a random word
 def generate_word():
-    return "footbarf"
+    """
+    Returns a random word from a random quote by Linus Torvalds
+    :return: returns a string containing a random quote
+    """
+
+    word_not_found = True
+    quote = "foobar"  # default word in case we couldn't find a word
+    while word_not_found:
+        try:
+            quote = random.choice(wikiquote.quotes("Linus Torvalds"))
+            print(quote)
+            quote = quote.split()
+            quote = ["".join(filter(str.isalpha, string)) for string in quote]
+            quote = list(filter(lambda x: len(x) > 4, quote))
+            quote = random.choice(quote)
+            print(quote)
+            quote = quote.lower()
+            word_not_found = False
+        except IndexError:
+            print("Couldn't find a word that was at least 5 letters long, trying again")
+
+    return quote
 
 
 # Gets the details of a hangman game for a given channel
 # This includes: mistake, guessed, hangmanWord
 # It should retrieve from the database and return the details in a dictionary
 def hangman_details(token, channel_id):
+    """
+    Gets details about the hangman game, including mistakes made, words guessed, and the HangmanWord
+    :param token: String for authorised user's identifier
+    :param channel_id: Integer for a specific channel
+    :return: dictionary containing hangman details
+    """
     channel = db.get_channels_by_key("channel_id", channel_id)[0]
 
     return channel["hangman"]
@@ -32,7 +67,6 @@ def hangman_details(token, channel_id):
 # Starts a game of hangman
 # Should mark a channel as having an active hangman game
 # Input error if a game is already active
-# TODO: determine if this is necessary
 def hangman_start(token, channel_id):
     channel = db.get_channels_by_key("channel_id", channel_id)[0]
 
@@ -44,7 +78,8 @@ def hangman_start(token, channel_id):
         "guessed": [" "],
         "hangmanWord": generate_word(),
         "guess": "",
-        "active": True
+        "active": True,
+        "is_winner": False
     }
 
     return channel["hangman"]
@@ -71,6 +106,9 @@ def hangman_guess(token, channel_id, guess_letter):
     if guess_letter in hangman["guessed"]:
         raise InputError("You have already guessed this letter")
 
+    if not guess_letter or not str.isalpha(guess_letter):
+        raise InputError("Incorrect letter passed in")
+
     # Add guess_letter to the guessed string
     hangman["guessed"] += guess_letter
 
@@ -85,6 +123,7 @@ def hangman_guess(token, channel_id, guess_letter):
     if hangman["mistake"] >= 10 or hasWonHangman(hangman["hangmanWord"], hangman["guessed"]):
         hangman["active"] = False
 
-    # TODO: check if we have guessed the word already
+    if hasWonHangman(hangman["hangmanWord"], hangman["guessed"]):
+        hangman["is_winner"] = True
 
     return hangman
